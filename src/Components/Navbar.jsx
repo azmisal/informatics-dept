3dad46cb869753edadf2d230b6e0d8a573a2dece
azmisal <b20cs33@gectcr.ac.in>
2024-05-09 00:18:19 +0530

    something to push

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Logo from "../Assets/logo.png"
+import "./Navbar.css"
+const Navbar = () => {
+  return (
+    <div className='flex items-center justify-center bg-black pt-5 pb-8 '>
+      <div className="innerBar pr-10 pl-10 flex flex-row h-10 w-3/4 justify-between items-center rounded-3xl">
+        <div className="logo">           
+            <img src={Logo} alt="" className='w-20'/>
+
+
+        </div>
+        <div className="navLinks ">
+                <ul className="flex items-center justify-center">
+                    <li className="navlinks cursor-pointer mr-10 font-poppins text-xs font-medium leading-6 text-left ">HOME</li>
+                    <li className="navlinks cursor-pointer mr-10 font-poppins text-xs font-medium leading-6 text-left">ABOUT</li>
+                    <li className="navlinks cursor-pointer mr-10 font-poppins text-xs font-medium leading-6 text-left">ACADEMICS</li>
+                    <li className="navlinks cursor-pointer mr-10 font-poppins text-xs font-medium leading-6 text-left">PEOPLE</li>
+                    <li className="navlinks cursor-pointer mr-10 font-poppins text-xs font-medium leading-6 text-left">RESEARCH</li>
+                    <li className="navlinks cursor-pointer font-poppins text-xs font-medium leading-6 text-left">GALLERY</li>
+
+                </ul>
+            </div>
+        </div>
+     </div>
+  )
+}
+
+export default Navbar
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./Home.css"
+const Home = () => {
+  return (
+    <div className='Home w-screen h-screen '>
+
+    </div>
+  )
+}
+
+export default Home
